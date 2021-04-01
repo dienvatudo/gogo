4bc2ad58c6d2fcf808ce6ad55f50d834408715f3
dienvatudo <dienvatudo@gmail.com>
2021-04-01 23:05:50 +0700

    commit source structure

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const isMale = true;
+  const me = {
+    name: 'Hung',
+    age: '24',
+  };
+  const colorList = ['red', 'blue', 'green'];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          DienVaTudo
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Xin chao
         </a>
+        {isMale ? <p>Nam</p> : <p>Nu</p>}
+        {/* {isMale && <p>Name</p>}
+        {!isMale && <p>Nu</p>}
+        {isMale && (
+          <>
+            <p>{me.name} </p> <p>{me.age}</p> <p>Name3</p>{' '}
+          </>
+        )} */}
+
+        <ul>
+          {colorList.map((color) => {
+            return (
+              <li key={color} style={{ color }}>
+                {color}
+              </li>
+            );
+          })}
+        </ul>
       </header>
     </div>
   );
---
+++ src/features/Todo/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoFeature.propTypes = {};
+
+function TodoFeature(props) {
+  return <div>Todo Feature</div>;
+}
+
+export default TodoFeature;
